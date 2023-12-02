cbba318796eeeda758b04a50cda32d6c1ea6ac22
AnuDB <vashisthazan@gmail.com>
2023-12-02 16:11:16 -0500

    add apps

--- app.py
+++ app.py
@@ -1,12 +1,36 @@
+# Import necessary libraries
 from flask import Flask, request, jsonify
+from joblib import load
 
+# Initialize Flask application
+app = Flask(__name__)
 
-app=Flask(__name__)
+# Load the Ridge Regression model
+model = load('ridge_model.joblib')
 
+# Define the home route
 @app.route('/')
 def home():
-    return "Hello, World"
+    return "Hello, World!"
 
-if __name__== '__main__':
+# Define the prediction endpoint
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Get data from POST request
+    data = request.get_json()
+
+    # Ensure that the 'features' key is present in the JSON request
+    if 'features' not in data:
+        return jsonify({"error": "Missing features in request"}), 400
+
+    try:
+        # Make a prediction using the model
+        prediction = model.predict([data['features']])
+        # Send back the prediction as a JSON
+        return jsonify(prediction=prediction[0])
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+# Run the Flask app
+if __name__ == '__main__':
     app.run(debug=True)
-    
