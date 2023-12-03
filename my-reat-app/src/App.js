99149b43d6b3e865f9dcaeb8eefb64acf38e5e25
AnuDB <vashisthazan@gmail.com>
2023-12-03 01:48:43 -0500

    improve the design of the page

--- my-reat-app/src/App.js
+++ my-reat-app/src/App.js
@@ -1,13 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
 import React from 'react';
+import './App.css';
 import PredictionForm from './PredictionForm';
+import { createTheme, ThemeProvider} from '@mui/material/styles';
+import {Typography, Card, CardContent } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#556cd6',
+    },
+    // Define other theme properties as needed
+  },
+});
 
 function App() {
   return (
-    <div className='App'>
-      <PredictionForm/>
+    <ThemeProvider theme={theme}>
+      <div className='App'>
+        <div className="left-align">
+          <Typography variant='h2' component="h1" guttertop>
+            House Price predictor using California 190 sensus dataset
+          </Typography>
+          <Card variant="outlined" style={{marginBottom: '20px', maxWidth: 800}}>
+            <CardContent>
+              <Typography variant="h5" component="h2" gutterBottom>
+                About the Dataset
+              </Typography>
+              <Typography variant="body1">
+                The dataset used for predicting house prices includes several features:
+                median income, housing age, average rooms, average bedrooms, population,
+                average occupancy, and location (latitude and longitude). These features
+                are used to train the Ridge Regression model which predicts the median
+                house value for California districts, derived from the 1990 census data.
+              </Typography>
+            </CardContent>
+          </Card>
+        </div>
+        <PredictionForm/>
       </div>
+    </ThemeProvider>
   );
 }
 
--- my-reat-app/src/PredictionForm.js
+++ my-reat-app/src/PredictionForm.js
@@ -1,99 +1,171 @@
 import React, {useState} from 'react';
 import axios from 'axios';
+import { TextField, Button} from '@mui/material';
 
 function PredictionForm(){
     const [inputs, setInputs]= useState({});
+    const [errors, setErrors]= useState({});
     const [prediction, setPrediction] = useState("");
 
     const handleChange = (event) =>{
         const name = event.target.name;
         const value = event.target.value;
-        setInputs(values => ({...values, [name]: value})) 
+        
+        if (name==="Longitude") {
+            if (value > -114 || value < -124.8){
+                setErrors(prevErrors=>({
+                    ...prevErrors,
+                    Longitude: "Longitude must be between -114 and -124. These values represent California geographic boundaries "
+                }));
+            } else{
+                setErrors(prevErrors => ({
+                    ...prevErrors,
+                    Longitude: null
+                }));
+            }
+        }
+
+        if (name==="Latitude") {
+            if (value > 42 || value < 32){
+                setErrors(prevErrors=>({
+                    ...prevErrors,
+                    Latitude: "Latitude must be between 32 and 42. These values represent California geographic boundaries "
+                }));
+            } else{
+                setErrors(prevErrors => ({
+                    ...prevErrors,
+                    Latitude: null
+                }));
+            }
+        }
+
+        
+
+        setInputs(values => ({...values, [name]: value}));
     }
 
     const handleSubmit = async (event) => {
         event.preventDefault();
+        if(Object.values(errors).some(error =>error)){
+            return;
+        }
         try {
+            // Transform 'MedInc' before sending
+            const transformedInputs  = {
+                ...inputs,
+                MedInc: inputs.MedInc ? parseFloat(inputs.MedInc) / 10000 : 0
+            };
             const response = await axios.post('http://localhost:5000/predict', {
-                features: Object.values(inputs)
+                features: Object.values(transformedInputs)
             });
-            setPrediction(response.data.prediction);
+
+            const scaledPrediction = response.data.prediction * 100000;
+            setPrediction(Math.round(scaledPrediction));
         } catch (error) {
             console.error("There was an error!", error);
         }
     } 
+
     return (
         <div>
             <form onSubmit={handleSubmit}>
 
-<input
-                    type="text"
+                <TextField
+                    type= "number"
+                    label="Median income for households"
+                    variant="outlined"                    
                     name="MedInc"
-                    placeholder="Median Income"
                     value={inputs.MedInc|| ""}
                     onChange={handleChange}
+                    margin= "normal"
+                    helperText="Enter amount in full format (e.g., 45000 )"
                 />
-<input
-                    type="text"
+
+                <TextField
+                    label="Median house age "
+                    variant="outlined"                    
                     name="HouseAge"
-                    placeholder="House Age"
                     value={inputs.HouseAge|| ""}
                     onChange={handleChange}
+                    margin= "normal"
                 />
-<input
-                    type="text"
+                <TextField
+                    label="Average number of rooms "
+                    variant="outlined"                    
                     name="AveRooms"
-                    placeholder="Average Rooms"
                     value={inputs.AveRooms|| ""}
                     onChange={handleChange}
+                    margin= "normal"
                 />
 
-
-<input
-                    type="text"
+                <TextField
+                    label="Average number of bedrooms "
+                    variant="outlined"                    
                     name="AveBedrms"
-                    placeholder="Nomber of Bedrooms"
                     value={inputs.AveBedrms|| ""}
                     onChange={handleChange}
+                    margin= "normal"
                 />
-
-<input
-                    type="text"
+                
+                <TextField
+                    label="block group population"
+                    variant="outlined"                    
                     name="Population"
-                    placeholder="Population"
                     value={inputs.Population|| ""}
                     onChange={handleChange}
+                    margin= "normal"
+                    helperText = "give an estimation of the population in the block. Usually, it range from 100 to 33000 habitants"
                 />
 
-<input
-                    type="text"
+                <TextField
+                    label="Average number of household members"
+                    variant="outlined"                    
                     name="AveOccup"
-                    placeholder="Average occupation"
                     value={inputs.AveOccup|| ""}
                     onChange={handleChange}
+                    margin= "normal"
                 />
 
-<input
-                    type="text"
+                <TextField
+                    type="number"
+                    inputProps={{
+                        min:"32",
+                        max:"42"
+                    }}
+                    label="block group Latitude"
+                    variant="outlined"                    
                     name="Latitude"
-                    placeholder="Latitude"
                     value={inputs.Latitude|| ""}
                     onChange={handleChange}
+                    margin= "normal"
+                    error={!!errors.Latitude}
+                    helperText={errors.Latitude}
                 />
-
-<input
-                    type="text"
+                <TextField
+                    type="number"
+                    inputProps={{
+                        min:"-124",
+                        max:"-114"
+                    }}
+                    label="block group longitude"
+                    variant="outlined"                    
                     name="Longitude"
-                    placeholder="Longitude"
                     value={inputs.Longitude|| ""}
                     onChange={handleChange}
+                    margin= "normal"
+                    error={!!errors.Longitude}
+                    helperText={errors.Longitude}
                 />
 
-
-                {/* Repeat for as many features as your model expects */}
-                <input type="submit" value="Predict" />
+                <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={handleSubmit}>
+                    Predict
+                </Button>
+                
             </form>
-            {prediction && <p>Prediction: {prediction}</p>}
+            {prediction && <p> The average price of an house given these features is {prediction}$</p>}
         </div>
     );
 }
